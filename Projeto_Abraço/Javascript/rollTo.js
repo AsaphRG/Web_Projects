5d9a701b7744c38f125a00c9d4fecc45d0febacc
AsaphRG <asaphresina1215@gmail.com>
2023-01-02 21:06:42 -0300

    Finalizar o css e iniciar o js configurando o roll.

--- Projeto_Abraço/Javascript/rollTo.js
+++ Projeto_Abraço/Javascript/rollTo.js
@@ -1,3 +1,22 @@
-function rollToPosition() {
+function rollToPosition(pos) {
+    posX = 0;
+    posY = 0;
+    
+    if (pos === "home") {
+        posY = 0;
+    } else if (pos === "intro") {
+        posY = 738;
+    } else if (pos === "projeto") {
+        posY = 1339;
+    } else if (pos === "contato") {
+        posY = 2345;
+    } else if (pos === "colab") {
+        posY = 2907;
+    };
 
+    window.scrollTo({
+        top: posY,
+        left: posX,
+        behavior: "smooth"
+    });
 }
